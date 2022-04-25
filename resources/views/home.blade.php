209ff661ed82b52c7a5135ed97d6e4e7d09fe116
oliferosadana <oliferosadana@gmail.com>
2022-04-25 13:08:35 +0700

    laravel topup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- custom -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="section">
+            <div class="row">
+                <header>
+                    <img src="image/index.png">
+                </header>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                    <div class="card">
+                        <img src="image/index.png">
+                        <a href="#">Top Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <h3>Metode Pembayaran</h3>
+        </div>
+        <div class="footer">
+            <h3>oodanastore.com</h3>
+        </div>
+        <div class="bot-nav">
+            <!-- <ul> -->
+                <li><a href="">Home</a></li>
+                <li><a href="">Riwayat</a></li>
+                <li><a href="">Akun</a></li>
+            <!-- </ul> -->
+        </div>
+    </div>
+</body>
+</html>
